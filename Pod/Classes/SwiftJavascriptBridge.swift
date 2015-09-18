9bc3cc82c9d429196ebd52803c8229572e5e70e3
gmontesTechnisys <gastonmontes@hotmail.com>
2015-09-18 17:31:59 -0300

    SwiftJavascriptBridge add handler method.

--- Pod/Classes/SwiftJavascriptBridge.swift
+++ Pod/Classes/SwiftJavascriptBridge.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import WebKit
+import UIKit
+
+class SwiftJavascriptBridge: NSObject, WKScriptMessageHandler {
+    
+    // MARK: - Vars.
+    private var jsWebView: WKWebView?
+    private let jsWebViewConfiguration = WKWebViewConfiguration()
+    private var handlersDictionary = NSMutableDictionary()
+    
+    // MARK: - Initialization.
+    init(urlString urlStr: String) {
+        super.init()
+        self.initializeWebViewWithURL(urlStr)
+    }
+    
+    // MARK: - Web view methods.
+    private func initializeWebViewWithURL(urlString: String) {
+        self.jsWebView = WKWebView(frame: CGRectMake(0, 0, 0, 0), configuration: self.jsWebViewConfiguration)
+        
+        let url     = NSURL(string: urlString)
+        let request = NSURLRequest(URL: url!)
+        self.jsWebView!.loadRequest(request);
+    }
+    
+    // MARK: - WKScriptMessageHandler implementation.
+    func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
+        // The name of the message handler to which the message is sent.
+        let messageHandlerName = String(message.name)
+        let objectHandler: AnyObject? = self.handlersDictionary.objectForKey(messageHandlerName)
+        let handlerSelector = Selector(messageHandlerName);
+        
+        if (objectHandler?.respondsToSelector(handlerSelector) == true) {
+            objectHandler!.respondsToSelector(handlerSelector)
+        } else {
+            NSLog("Cannot handler function: %@\n", messageHandlerName);
+        }
+    }
+    
+    // MARK: - Bridge creation.
+    class func bridgeForURLString(urlString: String) -> AnyObject {
+        let bridge: SwiftJavascriptBridge = SwiftJavascriptBridge(urlString: urlString)
+        return bridge;
+    }
+    
+    // MARK: - Public methods.
+    func bridgeAddHandler(objectHandler: AnyObject, handlerName: String) {
+        self.handlersDictionary.setObject(objectHandler, forKey: handlerName)
+        self.jsWebViewConfiguration.userContentController.addScriptMessageHandler(self, name: handlerName)
+        self.jsWebView = WKWebView(frame: CGRectMake(0, 0, 0, 0), configuration: self.jsWebViewConfiguration)
+    }
+    
+    func bridgeRemoveHandler() {
+        
+    }
+    
+    func bridgeCallHandler() {
+        
+    }
+}
