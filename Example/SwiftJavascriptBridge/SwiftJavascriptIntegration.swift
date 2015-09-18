66f235333a72913783b2413b2c86ab080095cb5d
gmontesTechnisys <gastonmontes@hotmail.com>
2015-09-18 17:56:19 -0300

    Add Example view controller.

---
+++ Example/SwiftJavascriptBridge/SwiftJavascriptIntegration.swift
@@ -0,0 +1,9 @@
+//
+//  SwiftJavascriptIntegration.swift
+//  SwiftJavascriptBridge
+//
+//  Created by Gaston Montes on 18/9/15.
+//  Copyright © 2015 CocoaPods. All rights reserved.
+//
+
+import Foundation
