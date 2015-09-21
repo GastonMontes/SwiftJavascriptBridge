b40ade54be8d9f633dbe678f9d9b7b99a607a192
Gaston Montes <gastonmontes@hotmail.com>
2015-09-21 19:24:37 -0300

    Add load script from url method.

--- Pod/Classes/SwiftJavascriptBridge.swift
+++ Pod/Classes/SwiftJavascriptBridge.swift
@@ -13,28 +13,10 @@ import UIKit
 public class SwiftJavascriptBridge: NSObject, WKScriptMessageHandler {
     
     // MARK: - Vars.
-    private var jsWebView: WKWebView?
     private let jsWebViewConfiguration = WKWebViewConfiguration()
+    private var jsWebView: WKWebView?
+    private var scriptURLString: String?
     private var handlersDictionary = NSMutableDictionary()
-    
-    // MARK: - Initialization.
-    init(urlString urlStr: String) {
-        super.init()
-        self.initializeWebViewWithURL(urlStr)
-    }
-    
-    // MARK: - Web view methods.
-    private func initializeWebViewWithURL(urlString: String) {
-        self.jsWebView = WKWebView(frame: CGRectMake(0, 0, 0, 0), configuration: self.jsWebViewConfiguration)
-        let url     = NSURL(string: urlString)
-        
-        if (url != nil) {
-            let request = NSURLRequest(URL: url!)
-            self.jsWebView!.loadRequest(request);
-        } else {
-            NSLog ("Bridge URL is not a valid URL: %@\n", urlString)
-        }
-    }
     
     // MARK: - WKScriptMessageHandler implementation.
     public func userContentController(userContentController: WKUserContentController, didReceiveScriptMessage message: WKScriptMessage) {
@@ -44,32 +26,43 @@ public class SwiftJavascriptBridge: NSObject, WKScriptMessageHandler {
         let handlerSelector = Selector(messageHandlerName);
         
         if (objectHandler?.respondsToSelector(handlerSelector) == true) {
-            objectHandler!.respondsToSelector(handlerSelector)
+            objectHandler!.performSelector(handlerSelector)
         } else {
             NSLog("Cannot handler function: %@\n", messageHandlerName);
         }
     }
     
     // MARK: - Bridge creation.
-    public class func bridgeForURLString(urlString: String) -> SwiftJavascriptBridge {
-        let bridge: SwiftJavascriptBridge = SwiftJavascriptBridge(urlString: urlString)
+    public class func bridgeForURLString() -> SwiftJavascriptBridge {
+        let bridge: SwiftJavascriptBridge = SwiftJavascriptBridge()
         return bridge;
     }
     
     // MARK: - Public methods.
-    func bridgeAddHandler(objectHandler: AnyObject, handlerName: String) {
+    public func bridgeAddHandler(objectHandler: AnyObject, handlerName: String) {
         self.handlersDictionary.setObject(objectHandler, forKey: handlerName)
         self.jsWebViewConfiguration.userContentController.addScriptMessageHandler(self, name: handlerName)
-        self.jsWebView = WKWebView(frame: CGRectMake(0, 0, 0, 0), configuration: self.jsWebViewConfiguration)
     }
     
-    func bridgeRemoveHandler(handlerName: String) {
+    public func bridgeRemoveHandler(handlerName: String) {
         self.handlersDictionary.removeObjectForKey(handlerName);
         self.jsWebViewConfiguration.userContentController.removeScriptMessageHandlerForName(handlerName)
-        self.jsWebView = WKWebView(frame: CGRectMake(0, 0, 0, 0), configuration: self.jsWebViewConfiguration)
     }
     
-    func bridgeCallHandler(jsHandlerName: String) {
+    public func bridgeCallHandler(jsHandlerName: String) {
         
+    }
+    
+    public func bridgeLoadScriptFromURL(urlString : String) {
+        self.scriptURLString = urlString;
+        
+        let url = NSURL(string: self.scriptURLString!)
+        if (url != nil) {
+            self.jsWebView = WKWebView(frame: CGRectMake(0, 0, 0, 0), configuration: self.jsWebViewConfiguration)
+            let request = NSURLRequest(URL: url!)
+            self.jsWebView!.loadRequest(request);
+        } else {
+            NSLog ("Bridge URL is not a valid URL: %@\n", urlString)
+        }
     }
 }
